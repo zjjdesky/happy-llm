ada2e0c44fa2703d5ce2047a86aff54859802556
KMnO4-zx <1021385881@qq.com>
2025-06-18 12:34:52 +0800

    fix(download.py): 修复解压命令未指定目标目录的问题

--- docs/chapter5/code/download.py
+++ docs/chapter5/code/download.py
@@ -9,7 +9,7 @@ os.environ['HF_ENDPOINT'] = 'https://hf-mirror.com'
 # 下载预训练数据集
 os.system("modelscope download --dataset ddzhu123/seq-monkey mobvoi_seq_monkey_general_open_corpus.jsonl.tar.bz2 --local_dir your_local_dir")
 # 解压预训练数据集
-os.system("tar -xvf your_local_dir/mobvoi_seq_monkey_general_open_corpus.jsonl.tar.bz2")
+os.system("tar -xvf your_local_dir/mobvoi_seq_monkey_general_open_corpus.jsonl.tar.bz2 -C your_local_dir")
 
 # 下载SFT数据集
 os.system(f'huggingface-cli download --repo-type dataset --resume-download BelleGroup/train_3.5M_CN --local-dir BelleGroup')
